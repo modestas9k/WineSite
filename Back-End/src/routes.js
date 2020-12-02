a89e48f65b653a6bea11aa84aa78b99ebb97fc05
Modestas Skirius <mode@Modestass-Mini.lan>
2020-12-02 18:19:33 +0200

    post and get to wine_types table

--- Back-End/src/routes.js
+++ Back-End/src/routes.js
@@ -106,4 +106,45 @@ router.post("/login", middleware.validateUserData, (req, res) => {
   );
 });
 
+router.post("/addWineType", middleware.isLoggedIn, (req, res) => {
+  if (req.body.wineName && req.body.type && req.body.year && req.body.region) {
+    con.query(
+      `INSERT INTO wine_types (name, region, type, year) VALUES ('${
+        req.body.wineName
+      }', ${mysql.escape(req.body.region)}, ${mysql.escape(
+        req.body.type
+      )}, ${mysql.escape(req.body.year)})`,
+      (err) => {
+        if (err) {
+          console.log(err);
+          return res
+            .status(400)
+            .json({ msg: "Internal server error gathering wine details" });
+        } else {
+          return res
+            .status(200)
+            .json({ msg: "Wine type has been added successfully" });
+        }
+      }
+    );
+  } else {
+    return res
+      .status(400)
+      .json({ msg: "Wine data has been passed incorrectly" });
+  }
+});
+
+router.get("/allWineTypes", middleware.isLoggedIn, (req, res) => {
+  con.query(`SELECT * FROM wine_types`, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(400)
+        .json({ msg: "Internal server error gathering wine details" });
+    } else {
+      return res.status(200).json(result);
+    }
+  });
+});
+
 module.exports = router;
