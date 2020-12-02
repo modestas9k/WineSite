a9832e705e836a36236ba6c8f69e0af42c688c58
Modestas Skirius <mode@Modestass-Mini.lan>
2020-12-02 18:20:57 +0200

    add addWineTypes page

--- Fron-End/src/pages/AddWineType/AddWineType.jsx
+++ Fron-End/src/pages/AddWineType/AddWineType.jsx
@@ -1,10 +1,97 @@
-import React from "react";
-import { Section } from "../../components";
+import React, { useContext, useState } from "react";
+import { Section, Input, Button, Notification } from "../../components";
+import * as S from "./AddWineTypes.style";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function AddWineType() {
+  const [message, setMessage] = useState();
+  const [color, setColor] = useState();
+  const [fieldValues, setFieldValues] = useState();
+  const Auth = useContext(AuthContext);
+
+  function AddWine(data) {
+    fetch("http://localhost:8080/addWineType", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Auth.token}`,
+      },
+      body: JSON.stringify({
+        wineName: data.wineName,
+        type: data.type,
+        year: data.year,
+        region: data.region,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.msg === "Wine type has been added successfully") {
+          setMessage(data.msg);
+          setColor("green");
+        } else {
+          setMessage(data.msg);
+        }
+      });
+    console.log(data);
+  }
+
   return (
     <>
-      <Section background="e3e3e3">Add Wine Type</Section>
+      <Section>
+        <S.H1>Add Wine Types</S.H1>
+        <S.Form
+          onSubmit={(e) => {
+            e.preventDefault();
+            AddWine(fieldValues);
+          }}
+        >
+          <S.H3>Add a Wine Types</S.H3>
+          <S.P>A short explanation how to do this</S.P>
+          {message && <Notification color={color} message={message} />}
+          <Input
+            label="Wine Name"
+            placeholder="Vorat"
+            name="wineName"
+            type="text"
+            handleChange={(e) => {
+              setFieldValues({ ...fieldValues, wineName: e.target.value });
+            }}
+          />
+          <Input
+            label="Region"
+            placeholder="Lithuania"
+            name="region"
+            type="text"
+            handleChange={(e) => {
+              setFieldValues({ ...fieldValues, region: e.target.value });
+            }}
+          />
+          <S.Split>
+            <Input
+              label="Type"
+              placeholder="White"
+              name="type"
+              type="select"
+              SelectOptions={["Select wine type", "White", "Red"]}
+              handleChange={(e) => {
+                setFieldValues({ ...fieldValues, type: e.target.value });
+              }}
+            />
+            <Input
+              label="Year"
+              placeholder="2017"
+              name="year"
+              type="number"
+              handleChange={(e) => {
+                setFieldValues({ ...fieldValues, year: e.target.value });
+              }}
+            />
+          </S.Split>
+          <S.ButtonWrapper>
+            <Button color="primary">Add</Button>
+          </S.ButtonWrapper>
+        </S.Form>
+      </Section>
     </>
   );
 }
