3f9a74b1674cbec6021e43bd96ab5992f5237297
Modestas Skirius <mode@Modestass-Mini.lan>
2020-12-02 11:48:17 +0200

    add Globalstyle, AuthProvider

--- Fron-End/src/index.js
+++ Fron-End/src/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
+import AuthProvider from "./contexts/AuthContext";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
 import "normalize.css";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Helvetica, sans-serif;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme} >
-      <Routes />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
